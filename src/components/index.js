38f6e17aee735357b1171aa7ec87d60c31f57168
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-08-01 22:12:43 +0300

    fix bug

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export * from 'components/Phonebook.styled.jsx';
-export * from 'components/Phonebook/Phonebook';
 export * from 'components/Form/Form.jsx';
-export * from 'components/ContactsList/ContactsList.jsx'
-export * from 'components/Filter/Filter.jsx'
+export * from 'components/ContactsList/ContactsList.jsx';
+export * from 'components/Filter/Filter.jsx';
